6c73870f783cea970b89a7639b332e7993cf84d0
Taimur Tawil <taimurtawil@gmail.com>
2022-10-24 14:25:23 -0400

    Import on this image not working in render

--- client/src/components/WorkoutPost.vue
+++ client/src/components/WorkoutPost.vue
@@ -2,9 +2,9 @@
 import WorkoutViewVue from '@/views/WorkoutView.vue';
 import type { Workout } from '@/stores/types';
 import { setMapStoreSuffix } from 'pinia';
-    const props = defineProps({
-        workout: Object as () => Workout,
-    });
+    // const props = defineProps({
+    //     workout: Object as () => Workout,
+    // });
     
 
 </script>
@@ -19,12 +19,12 @@ import { setMapStoreSuffix } from 'pinia';
         <div class="media-content">
             <div class="content">
                 <p>
-                    <strong>{{props.workout.title}}</strong>
+                    <strong>{{/*props.workout.title*/}}</strong>
                     <br>
-                    {{props.workout.description}}
+                    {{/*props.workout.description*/}}
                     <br>
-                    <small><a>Like</a> · <a>Reply</a> · {{props.workout.date}}</small><br>
-                    <img src={{props.workout.image}} alt="uploaded img"/>
+                    <small><a>Like</a> · <a>Reply</a> · {{/*props.workout.date*/}}</small><br>
+                    <img src={{/*props.workout.image*/}} alt="uploaded img"/>
                 </p>
             </div>
         </div>
